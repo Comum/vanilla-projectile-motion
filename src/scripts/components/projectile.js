938eeb8bd469420aa385245def0ce12fe8b7e67b
Comum <miguel.rib.20@gmail.com>
2020-01-12 19:13:49 +0000

    feat: make the projectiles visible

--- src/scripts/components/projectile.js
+++ src/scripts/components/projectile.js
@@ -3,14 +3,20 @@ import utils from "../utils/utils";
 
 class Projectile {
   constructor(initialPositionX, initialPositionY) {
-    this.initialPositionX = initialPositionX;
-    this.initialPositionY = initialPositionY;
+    this.positionX = initialPositionX;
+    this.positionY = initialPositionY;
 
     this.colour = utils.generateRandomColour();
     this.size = utils.generateRandomNumber(
       constants.projectileMinSize,
       constants.projectileMinSize
     );
+  }
+
+  update() {
+    console.log("Projectile.update", this);
+    // this.positionX = this.calculateNewPosition("x");
+    // this.positionY = this.calculateNewPosition("y");
   }
 }
 
--- src/scripts/components/projectileContainer.js
+++ src/scripts/components/projectileContainer.js
@@ -13,7 +13,10 @@ class ProjectileContainer {
 
     this.container.appendChild(this.canvas);
     this.container.addEventListener("click", this.onClickContainer.bind(this));
-    this.clearButton.addEventListener("click", this.onClickClearButton);
+    this.clearButton.addEventListener(
+      "click",
+      this.onClickClearButton.bind(this)
+    );
 
     this.updateBound = this.update.bind(this);
     requestAnimationFrame(this.updateBound);
@@ -26,6 +29,9 @@ class ProjectileContainer {
 
   onClickContainer(event) {
     const { clientX, clientY } = event;
+    // initial value needs to be relative to the box and not the screen
+    // use get boundingClientRect in the constructor and subtract the offset from the
+    // values above
 
     this.projectiles.push(new Projectile(clientX, clientY));
     // remove next line
@@ -33,14 +39,36 @@ class ProjectileContainer {
   }
 
   onClickClearButton() {
+    console.log("clear", this);
     this.projectiles = [];
+    // remove next line
+    requestAnimationFrame(this.updateBound);
   }
 
   update() {
+    console.log("update");
     this.ctx.clearRect(0, 0, this.width, this.height);
 
+    if (this.projectiles.length === 0) {
+      console.log("should clear canvas");
+    }
+
     for (const projectile of this.projectiles) {
-      console.log("projectile", projectile);
+      projectile.update();
+
+      this.ctx.save();
+      this.ctx.fillStyle = projectile.colour;
+      this.ctx.beginPath();
+      this.ctx.arc(
+        projectile.positionX,
+        projectile.positionY,
+        projectile.size,
+        0,
+        Math.PI * 2
+      );
+      this.ctx.closePath();
+      this.ctx.fill();
+      this.ctx.restore();
     }
     //requestAnimationFrame(this.updateBound);
   }
